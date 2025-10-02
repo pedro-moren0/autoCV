61a7571d1fb816c2c45d55b5bd9d93407c3ff1eb
pedro <pedro.moreno@aluno.ufabc.edu.br>
2025-10-02 17:12:02 -0300

    turning repo into flake + other changes

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all:
 	latexmk -pdf ${NAME}.tex
 
 clean:
-	rm -f ${NAME}.aux ${NAME}.bbl ${NAME}.bcf ${NAME}.fdb_latexmk ${NAME}.fls ${NAME}.log ${NAME}.out ${NAME}.run.xml ${NAME}.blg ${NAME}.toc *\~
+	rm -f ${NAME}.aux ${NAME}.bbl ${NAME}.bcf ${NAME}.fdb_latexmk ${NAME}.fls ${NAME}.log ${NAME}.out ${NAME}.run.xml ${NAME}.blg ${NAME}.toc ${NAME}.synctex.gz *\~
 
 distclean: clean
 	rm -f ${NAME}.pdf
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A LaTeX template with my neovim configuration.";
+
+  inputs = {
+    mynixvim.url = "github:pedro-moren0/neovim-config";
+  };
+
+  outputs =
+    { nixpkgs, mynixvim, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      nvim = mynixvim.outputs.packages.${system}.default.extend {
+        plugins.vimtex.enable = true;
+        plugins.vimtex.texlivePackage = null;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          nvim
+        ];
+      };
+    };
+}
